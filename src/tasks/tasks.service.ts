8aa92570dbcc9fc4a95c19e49905fcad9d2e6735
Hardik Kundalia <hardik.kundalia@contentstack.com>
2022-01-12 18:53:53 +0530

    Added User Object to UpdateTaskStatus Controller & Service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -18,7 +18,7 @@ export class TasksService {
     return this.tasksRepository.getTasks(filterDto, user);
   }
 
-  async getTaskById(id: string, user?: User): Promise<Task> {
+  async getTaskById(id: string, user: User): Promise<Task> {
     const found = await this.tasksRepository.findOne({ where: { id, user } });
 
     if (!found) {
@@ -39,8 +39,12 @@ export class TasksService {
     }
   }
 
-  async updateTaskStatus(id: string, status: TaskStatus): Promise<Task> {
-    const task = await this.getTaskById(id);
+  async updateTaskStatus(
+    id: string,
+    status: TaskStatus,
+    user: User,
+  ): Promise<Task> {
+    const task = await this.getTaskById(id, user);
 
     task.status = status;
     await this.tasksRepository.save(task);
