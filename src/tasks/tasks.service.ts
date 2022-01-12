42c2854332fea3ed069d1d5a5dcbc7d9283576a1
Hardik Kundalia <hardik.kundalia@contentstack.com>
2022-01-12 18:58:25 +0530

    Added User Object to DeleteTask Controller & Service

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -19,7 +19,7 @@ export class TasksService {
   }
 
   async getTaskById(id: string, user: User): Promise<Task> {
-    const found = await this.tasksRepository.findOne({ where: { id, user } });
+    const found = await this.tasksRepository.findOne({ where: { id, user } }); // same as { id, user }
 
     if (!found) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
@@ -32,8 +32,8 @@ export class TasksService {
     return this.tasksRepository.createTask(createTaskDto, user);
   }
 
-  async deleteTask(id: string): Promise<void> {
-    const result = await this.tasksRepository.delete(id);
+  async deleteTask(id: string, user: User): Promise<void> {
+    const result = await this.tasksRepository.delete({ id, user }); // same as { where: { id, user } }
     if (result.affected === 0) {
       throw new NotFoundException(`Task with ID "${id}" not found`);
     }
